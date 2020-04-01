a3470639932d171b49d07c89807b6513d651bb8a
James Lenahan <james.k.lenahan@gmail.com>
2020-04-01 18:22:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+ 
+  products.forEach(product => {
+    totalPrice += product.price;
+  });
+ 
+  return totalPrice;
+}
+ 
+console.log(getTotalAmountForProducts(products)); // prints 33.46
