a63cc8d01899ebedfb462b177ca9ec69ee0f1db5
James Lenahan <james.k.lenahan@gmail.com>
2020-04-01 18:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-function getTotalAmountForProducts(products) {
-  let totalPrice = 0;
- 
-  products.forEach(product => {
-    totalPrice += product.price;
-  });
- 
-  return totalPrice;
-}
- 
-console.log(getTotalAmountForProducts(products)); // prints 33.46
+// Code your solution here
+let totalBatteries = batteryBatches.reduce( (total, batteryCount) => {
+    return total + batteryCount;
+})
